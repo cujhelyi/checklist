79f2efc1f2f48a4cc976ca907928a63841cc9268
cujhelyi <cujhelyi1479@conestogac.on.ca>
2021-09-03 12:57:43 -0400

    test commit

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+    //this is the input box branch
+
+    public function page(){
+        return $this->belongsTo(Page::class);
+    }
+
 }
